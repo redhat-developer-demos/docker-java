c431a2bd7581778810a5eafa9d471b3d6452609d
Rafael Benevides <rafabene@gmail.com>
2015-11-09 13:37:31 -0500

    Fix #14 - Include Kubernetes, Remove Swarm, Update to Docker 1.9

--- instructor/push-images-to-registry.sh
+++ instructor/push-images-to-registry.sh
@@ -1,73 +1,73 @@
 # Docker hello world
 docker pull hello-world
-docker tag hello-world localhost:5000/hello-world
-docker push localhost:5000/hello-world
+docker tag hello-world classroom.example.com:5000/hello-world
+docker push classroom.example.com:5000/hello-world
 
 # Fedora
 docker pull fedora
-docker pull fedora:21
-docker tag fedora localhost:5000/fedora
-docker tag fedora:21 localhost:5000/fedora:21
-docker push localhost:5000/fedora
-docker push localhost:5000/fedora:21
+docker pull fedora:22
+docker tag fedora classroom.example.com:5000/fedora
+docker tag fedora:22 classroom.example.com:5000/fedora:22
+docker push classroom.example.com:5000/fedora
+docker push classroom.example.com:5000/fedora:22
 
 # Wildfly
 docker pull jboss/wildfly
-docker tag jboss/wildfly localhost:5000/wildfly
-docker push localhost:5000/wildfly
+docker tag jboss/wildfly classroom.example.com:5000/wildfly
+docker push classroom.example.com:5000/wildfly
 
 # Managed WildFly
-docker tag instructor/wildfly-management localhost:5000/wildfly-management
-docker push localhost:5000/wildfly-management
+docker tag instructor/wildfly-management classroom.example.com:5000/wildfly-management
+docker push classroom.example.com:5000/wildfly-management
 
 # Ticket-monster+PGSQ+WildFly
-docker tag instructor/ticketmonster-pgsql-wildfly localhost:5000/ticketmonster-pgsql-wildfly
-docker push localhost:5000/ticketmonster-pgsql-wildfly
+docker tag instructor/ticketmonster-pgsql-wildfly classroom.example.com:5000/ticketmonster-pgsql-wildfly
+docker push classroom.example.com:5000/ticketmonster-pgsql-wildfly
 
 # Postgres
 docker pull postgres
-docker tag postgres localhost:5000/postgres
-docker push localhost:5000/postgres
+docker tag postgres classroom.example.com:5000/postgres
+docker push classroom.example.com:5000/postgres
 
 # Modcluster
 docker pull goldmann/mod_cluster
-docker tag goldmann/mod_cluster localhost:5000/mod_cluster
-docker push localhost:5000/mod_cluster
+docker tag goldmann/mod_cluster classroom.example.com:5000/mod_cluster
+docker push classroom.example.com:5000/mod_cluster
 
 #Java EE 7 hol
 docker pull arungupta/javaee7-hol
-docker tag arungupta/javaee7-hol localhost:5000/javaee7-hol
-docker push localhost:5000/javaee7-hol
+docker tag arungupta/javaee7-hol classroom.example.com:5000/javaee7-hol
+docker push classroom.example.com:5000/javaee7-hol
 
 # MySQL
 docker pull mysql:latest
-docker tag mysql localhost:5000/mysql
-docker push localhost:5000/mysql
+docker tag mysql classroom.example.com:5000/mysql
+docker push classroom.example.com:5000/mysql
 
 # WildFly MySQL
 docker pull arungupta/wildfly-mysql-javaee7
-docker tag arungupta/wildfly-mysql-javaee7 localhost:5000/wildfly-mysql-javaee7
-docker push localhost:5000/wildfly-mysql-javaee7
+docker tag arungupta/wildfly-mysql-javaee7 classroom.example.com:5000/wildfly-mysql-javaee7
+docker push classroom.example.com:5000/wildfly-mysql-javaee7
 
 # Docker Swarm
 docker pull swarm
-docker tag swarm localhost:5000/swarm
-docker push localhost:5000/swarm
+docker tag swarm classroom.example.com:5000/swarm
+docker push classroom.example.com:5000/swarm
 
 #OpenShift Origin
 #docker pull openshift/origin
-#docker tag openshift/origin localhost:5000/openshift-origin
-#docker push localhost:5000/openshift-origin
+#docker tag openshift/origin classroom.example.com:5000/openshift-origin
+#docker push classroom.example.com:5000/openshift-origin
 #docker pull openshift/origin-docker-registry
-#docker tag openshift/origin-docker-registry localhost:5000/openshift-origin-docker-registry
-#docker push localhost:5000/openshift-origin-docker-registry
+#docker tag openshift/origin-docker-registry classroom.example.com:5000/openshift-origin-docker-registry
+#docker push classroom.example.com:5000/openshift-origin-docker-registry
 #docker pull openshift/origin-sti-builder
-#docker tag openshift/origin-sti-builder localhost:5000/openshift-origin-sti-builder
-#docker push localhost:5000/openshift-origin-sti-builder
+#docker tag openshift/origin-sti-builder classroom.example.com:5000/openshift-origin-sti-builder
+#docker push classroom.example.com:5000/openshift-origin-sti-builder
 #docker pull openshift/origin-deployer
-#docker tag openshift/origin-deployer localhost:5000/openshift-origin-deployer
-#docker push localhost:5000/openshift-origin-deployer
+#docker tag openshift/origin-deployer classroom.example.com:5000/openshift-origin-deployer
+#docker push classroom.example.com:5000/openshift-origin-deployer
 #docker pull arungupta/wildfly-mysql-javaee7:k8s
-#docker tag arungupta/wildfly-mysql-javaee7:k8s localhost:5000/wildfly-mysql-javaee7:k8s
-#docker push localhost:5000/wildfly-mysql-javaee7:k8s
+#docker tag arungupta/wildfly-mysql-javaee7:k8s classroom.example.com:5000/wildfly-mysql-javaee7:k8s
+#docker push classroom.example.com:5000/wildfly-mysql-javaee7:k8s
 
